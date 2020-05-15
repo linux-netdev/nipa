67336bf67bf1a6c7375488e370d8141dcafa7867
Jakub Kicinski <kuba@kernel.org>
2020-05-15 16:11:23 -0700

    add header inline test
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

---
+++ tests/patch/header_inline/header_inline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (c) 2020 Facebook
+
+inlines=$(git show -- '*.h' | grep -C1 -P '^\+static (?!inline).*\)$')
+
+if [ -z "$inlines" ]; then
+  exit 0
+else
+  echo -e "Detected static functions without inline keyword in C files\n$inlines" 1>&2
+  exit 1
+fi
