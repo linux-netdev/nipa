018120dd64637dbf4120dda4e8677b93322cc340
Jakub Kicinski <kuba@kernel.org>
2020-07-07 11:42:47 -0700

    make static inlines in C not error on old problems
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tests/patch/source_inline/source_inline.sh
+++ tests/patch/source_inline/source_inline.sh
@@ -4,10 +4,19 @@
 # Copyright (C) 2019 Netronome Systems, Inc.
 
 inlines=$(git show -- '*.c' | grep -i '^\+\(.*\W\|\)inline\W')
+new_inlines=$(git show -- '*.c' | grep -ic '^\+\(.*\W\|\)inline\W')
+old_inlines=$(git show -- '*.c' | grep -ic '^\-\(.*\W\|\)inline\W')
+
+echo "Was $old_inlines now: $new_inlines" >&$DESC_FD
 
 if [ -z "$inlines" ]; then
   exit 0
 else
   echo -e "Detected inline keyword in C files\n$inlines" 1>&2
-  exit 1
+
+  if [ $new_inlines -eq $old_inlines ]; then
+    exit 250
+  else
+    exit 1
+  fi
 fi
