096f19fee5bbc632a3e91edb522ec865e644f05a
Jakub Kicinski <kuba@kernel.org>
2024-01-14 11:59:56 -0800

    contest: ui: support filtering of results
    There's a lot of results now, support filtering them down.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- contest/contest.js
+++ contest/contest.js
@@ -13,15 +13,28 @@ function colorify_str(value)
 function load_result_table(data_raw)
 {
     var table = document.getElementById("results");
+    var result_filter = {
+	"pass": document.getElementById("pass").checked,
+	"skip": document.getElementById("skip").checked,
+	"warn": document.getElementById("warn").checked,
+	"fail": document.getElementById("fail").checked
+    };
+    var branch_filter = document.getElementById("branch").value;
+    var exec_filter = document.getElementById("executor").value;
 
+    // Remove all rows but first (leave headers)
+    $("#results tr").slice(1).remove();
     $.each(data_raw, function(i, v) {
-	v.start = new Date(v.start);
-	v.end = new Date(v.end);
-    });
+	$.each(v.results, function(j, r) {
+	    if (result_filter[r.result] == false)
+		return 1;
+	    if (branch_filter &&
+		branch_filter != v.branch)
+		return 1;
+	    if (exec_filter &&
+		exec_filter != v.executor)
+		return 1;
 
-    data_raw.sort(function(a, b){return b.end - a.end;});
-
-    $.each(data_raw, function(i, v) {
 	    var row = table.insertRow();
 
 	    var date = row.insertCell(0);
@@ -36,20 +49,72 @@ function load_result_table(data_raw)
 	    branch.innerHTML = v.branch;
 	    remote.innerHTML = v.remote;
 	    exe.innerHTML = v.executor;
-	    group.innerHTML = v.group;
-	    test.innerHTML = "<a href=\"" + v.link + "\">" + v.test + "</a>";
-	    res.innerHTML = colorify_str(v.result);
+	    group.innerHTML = r.group;
+	    test.innerHTML = "<a href=\"" + r.link + "\">" + r.test + "</a>";
+	    res.innerHTML = colorify_str(r.result);
+	});
     });
+}
+
+let loaded_data = null;
+
+function add_option_filter(data_raw, elem_id, field)
+{
+    var elem = document.getElementById(elem_id);
+    var values = new Set();
+
+    $.each(data_raw, function(i, v) {
+	values.add(v[field]);
+    });
+    for (const value of values) {
+	const opt = document.createElement('option');
+	opt.value = value;
+	opt.innerHTML = value;
+	elem.appendChild(opt);
+    }
+    elem.addEventListener("change", results_update);
 }
 
 function results_doit(data_raw)
 {
+    const ingredients = document.querySelectorAll("input[name=fl-state]");
+
+    for (const ingredient of ingredients) {
+	ingredient.addEventListener("change", results_update);
+    }
+
+    $.each(data_raw, function(i, v) {
+	v.start = new Date(v.start);
+	v.end = new Date(v.end);
+    });
+
+    data_raw.sort(function(a, b){return b.end - a.end;});
+
+    add_option_filter(data_raw, "branch", "branch");
+    add_option_filter(data_raw, "executor", "executor");
+
+    loaded_data = data_raw;
     load_result_table(data_raw);
+}
+
+function results_update()
+{
+    if (loaded_data != 1) {
+	load_result_table(loaded_data);
+    }
 }
 
 function do_it()
 {
-    $(document).ready(function() {
-        $.get("contest/all-results.json", results_doit)
-    });
+    if (loaded_data == null) {
+	loaded_data = 1;
+
+	$(document).ready(function() {
+            $.get("contest/all-results.json", results_doit)
+	});
+    } else if (loaded_data == 1) {
+	/* nothing, loading in progress */
+    } else {
+	load_result_table(loaded_data);
+    }
 }
