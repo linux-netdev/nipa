1e022a2cfbfebaf735dbbd7074ba9e473a9c41a8
Jakub Kicinski <kuba@kernel.org>
2024-03-28 13:18:25 -0700

    contest: gh: add a very simple GH connector
    Add a runner which pushes to a remote and expects GH
    to run some actions.
    
    The expectations is that there will be a permanently open
    PR from the branch to which we are pushing, and the push
    will make GH run actions. Which we can then fetch back.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

---
+++ contest/remote/gh.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+import configparser
+import datetime
+import os
+import requests
+import subprocess
+import sys
+import time
+
+from core import NipaLifetime
+from lib import Fetcher, CbArg
+
+"""
+[executor]
+name=
+group=
+test=
+[remote]
+branches=
+[local]
+tree_path=
+base_path=
+results_path=
+json_path=
+[www]
+url=
+
+[gh]
+token=api-token
+base=base/branch
+link=https://full/link
+out_remote=remote-name
+out_branch=remote-branch
+wait_first=secs-to-first-check
+wait_poll=secs-between-rechecks
+wait_max=secs-to-wait
+[ci]
+owner=gh-owner
+repo=gh-repo
+runs_ref=refs/pull/...
+"""
+
+def get(url, token):
+    headers = {"Accept": "application/vnd.github+json",
+               "X-GitHub-Api-Version": "2022-11-28",
+               "Authorization": token}
+    return requests.get(url, headers=headers)
+
+
+def get_results(config, cbarg, prev_run):
+    token = config.get('gh', 'token')
+    repo_url = f"https://api.github.com/repos/{config.get('ci', 'owner')}/{config.get('ci', 'repo')}"
+    ref = config.get('ci', 'runs_ref')
+
+    resp = get(repo_url + '/actions/runs', token)
+    runs = resp.json()
+    found = None
+    for run in runs.get('workflow_runs'):
+        if ref in [r['ref'] for r in run['referenced_workflows']]:
+            if found is None or found["id"] < run["id"]:
+                found = run
+    if found is None:
+        print("Run not found!")
+        return None
+    if prev_run == found["id"]:
+        print("Found old run:", prev_run)
+        return None
+    cbarg.prev_runid = found["id"]
+
+    resp = get(repo_url + f'/actions/runs/{found["id"]}/jobs', token)
+    jobs = resp.json()
+
+    if 'jobs' not in jobs:
+        print("bad jobs")
+        print(jobs)
+        return None
+
+    decoder = {
+        'success': 0,
+        'skipped': 1,
+        None: 2,
+        'failure': 3,
+        'cancelled': 4,
+        'unknown': 5,
+    }
+    encoder = {
+        0: 'pass',
+        1: 'pass',
+        2: None,
+        3: 'fail',
+        4: 'fail',
+        5: 'fail',
+    }
+
+    result = -1
+    for job in jobs["jobs"]:
+        c = job["conclusion"]
+        if job["conclusion"] in decoder:
+            result = max(result, decoder[c])
+        else:
+            print("Unknown result:", c)
+            result = 5
+    return encoder[result]
+
+
+def test_run(binfo, rinfo, cbarg, config, start):
+    tree_path = config.get('local', 'tree_path')
+    base = config.get('gh', 'base')
+
+    subprocess.run('git checkout ' + base, cwd=tree_path, shell=True, check=True)
+    res = subprocess.run('git merge ' + binfo['branch'], cwd=tree_path, shell=True)
+    if res.returncode != 0:
+        # If rerere fixed it, just commit
+        res = subprocess.run('git diff -s --exit-code', cwd=tree_path, shell=True)
+        if res.returncode != 0:
+            return 'skip'
+        subprocess.run('git commit --no-edit', cwd=tree_path, shell=True, check=True)
+
+    out_remote = config.get('gh', 'out_remote')
+    out_branch = config.get('gh', 'out_branch')
+
+    subprocess.run(f'git push -f {out_remote} HEAD:{out_branch}',
+                   cwd=tree_path, shell=True, check=True)
+
+    end = start + datetime.timedelta(seconds=config.getint('gh', 'wait_max'))
+    time.sleep(config.getint('gh', 'wait_first'))
+
+    prev_runid = 0
+    if hasattr(cbarg, "prev_runid"):
+        prev_runid = cbarg.prev_runid
+
+    while datetime.datetime.now() < end:
+        res = get_results(config, cbarg, prev_runid)
+        if res:
+            print("Got result:", res)
+            return res
+
+        print("Not completed, waiting")
+        time.sleep(config.getint('gh', 'wait_poll'))
+
+    return 'skip'
+
+
+def test(binfo, rinfo, cbarg):
+    start = datetime.datetime.now()
+    print("Run at", start)
+
+    cbarg.refresh_config()
+    config = cbarg.config
+
+    results_path = os.path.join(config.get('local', 'base_path'),
+                                config.get('local', 'results_path'),
+                                rinfo['run-cookie'])
+    os.makedirs(results_path)
+
+    res = test_run(binfo, rinfo, cbarg, config, start)
+
+    link = config.get('gh', 'link')
+    if hasattr(cbarg, "prev_runid"):
+        link = "https://github.com/" + \
+               config.get('ci', 'owner') + "/" + \
+               config.get('ci', 'repo') + "/" + \
+               "actions/runs/" + str(cbarg.prev_runid)
+
+    return [{'test': config.get('executor', 'test'),
+             'group': config.get('executor', 'group'),
+             'result': res, 'link': link}]
+
+
+def main() -> None:
+    cfg_paths = ['remote.config', 'gh.config']
+    if len(sys.argv) > 1:
+        cfg_paths += sys.argv[1:]
+
+    cbarg = CbArg(cfg_paths)
+    config = cbarg.config
+
+    base_dir = config.get('local', 'base_path')
+
+    life = NipaLifetime(config)
+
+    f = Fetcher(test, cbarg,
+                name=config.get('executor', 'name'),
+                branches_url=config.get('remote', 'branches'),
+                results_path=os.path.join(base_dir, config.get('local', 'json_path')),
+                url_path=config.get('www', 'url') + '/' + config.get('local', 'json_path'),
+                tree_path=config.get('local', 'tree_path'),
+                patches_path=config.get('local', 'patches_path', fallback=None),
+                life=life,
+                first_run=config.get('executor', 'init', fallback="continue"))
+    f.run()
+    life.exit()
+
+
+if __name__ == "__main__":
+    main()
