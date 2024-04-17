4cc6383e92282282f867a28b833d2a0062b02479
Jakub Kicinski <kuba@kernel.org>
2024-04-17 16:20:47 -0700

    series_format: fix cover letter in description
    We only check for pull cover not normal cover letter
    so the description ends up saying "(and no cover letter)"
    even if there was one.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tests/series/series_format/test.py
+++ tests/series/series_format/test.py
@@ -24,7 +24,9 @@ def patch_count(tree, thing, result_dir) -> Tuple[int, str]:
     if len(thing.patches) <= 15:
         return 0, ""
     if thing.cover_pull:
-        return 250, "Series longer than 15 patches"
+        return 250, "Series longer than 15 patches (PR)"
+    if thing.cover_letter:
+        return 1, "Series longer than 15 patches"
     # Really no good if there's no cover letter.
     return 1, "Series longer than 15 patches (and no cover letter)"
 
