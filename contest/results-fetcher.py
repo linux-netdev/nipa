1c1d96cbbaa9c9ba41421dbd135bd5399125542f
Jakub Kicinski <kuba@kernel.org>
2024-05-17 13:25:23 -0700

    contest: fetcher: populate CouchDB with the results
    Feed the results into CouchDB as we get them.
    For now produce all-results.json as well.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- contest/results-fetcher.py
+++ contest/results-fetcher.py
@@ -2,11 +2,13 @@
 # SPDX-License-Identifier: GPL-2.0
 
 import configparser
+import couchdb
 import datetime
 import json
 import os
 import requests
 import time
+import uuid
 
 
 """
@@ -20,6 +22,11 @@ remote_db=/path/to/db
 dir=/path/to/output
 url_pfx=relative/within/server
 combined=name-of-manifest.json
+[db]
+results-name=db-name
+branches-name=db-name
+user=name
+pwd=pass
 """
 
 
@@ -31,6 +38,71 @@ class FetcherState:
         # "fetched" is more of a "need state rebuild"
         self.fetched = True
 
+        user = self.config.get("db", "user")
+        pwd = self.config.get("db", "pwd")
+        server = couchdb.Server(f'http://{user}:{pwd}@127.0.0.1:5984')
+        self.res_db = server[self.config.get("db", "results-name", fallback="results")]
+        self.brn_db = server[self.config.get("db", "branches-name", fallback="branches")]
+
+    def _one(self, rows):
+        rows = list(rows)
+        if len(rows) != 1:
+            raise Exception("Expected 1 row, found", rows)
+        return rows[0]
+
+    def get_branch(self, name):
+        branch_info = self.brn_db.find({
+            'selector': {
+                'branch': name
+            }
+        })
+        return self._one(branch_info)
+
+    def get_wip_row(self, remote, run):
+        rows = self.res_db.find({
+            'selector': {
+                'branch': run["branch"],
+                'remote': remote["name"],
+                'executor': run["executor"],
+                'url': None
+            }
+        })
+        for row in rows:
+            return row
+
+    def insert_wip(self, remote, run):
+        existing = self.get_wip_row(remote, run)
+
+        branch_info = self.get_branch(run["branch"])
+
+        data = run.copy()
+        if existing:
+            data['_id'] = existing['_id']
+            data['_rev'] = existing['_rev']
+        else:
+            data['_id'] = uuid.uuid4().hex
+        data["remote"] = remote["name"]
+        when = datetime.datetime.fromisoformat(branch_info['date'])
+        data["start"] = str(when)
+        when += datetime.timedelta(hours=2, minutes=58)
+        data["end"] = str(when)
+        data["results"] = None
+
+        self.res_db.save(data)
+
+    def insert_real(self, remote, run):
+        existing = self.get_wip_row(remote, run)
+
+        data = run.copy()
+        if existing:
+            data['_id'] = existing['_id']
+            data['_rev'] = existing['_rev']
+        else:
+            data['_id'] = uuid.uuid4().hex
+        data["remote"] = remote["name"]
+
+        self.res_db.save(data)
+
 
 def write_json_atomic(path, data):
     tmp = path + '.new'
@@ -39,9 +111,11 @@ def write_json_atomic(path, data):
     os.rename(tmp, path)
 
 
-def fetch_remote_run(run_info, remote_state):
+def fetch_remote_run(fetcher, remote, run_info, remote_state):
     r = requests.get(run_info['url'])
     data = json.loads(r.content.decode('utf-8'))
+
+    fetcher.insert_real(remote, data)
 
     file = os.path.join(remote_state['dir'], os.path.basename(run_info['url']))
     with open(file, "w") as fp:
@@ -62,11 +136,13 @@ def fetch_remote(fetcher, remote, seen):
         if run['branch'] in remote_state['seen']:
             continue
         if not run['url']:    # Executor has not finished, yet
-            fetcher.fetched |= run['branch'] not in remote_state['wip']
+            if run['branch'] not in remote_state['wip']:
+                fetcher.insert_wip(remote, run)
+                fetcher.fetched = True
             continue
 
         print('Fetching run', run['branch'])
-        fetch_remote_run(run, remote_state)
+        fetch_remote_run(fetcher, remote, run, remote_state)
         fetcher.fetched = True
 
     with open(os.path.join(remote_state['dir'], 'results.json'), "w") as fp:
