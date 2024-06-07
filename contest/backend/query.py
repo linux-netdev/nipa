15d89a701fd5107b20eb967cdec6d208376fdacc
Jakub Kicinski <kuba@kernel.org>
2024-06-07 12:06:26 -0700

    contest: backend: switch queries to PostgreSQL
    Use PostgreSQL for queries. When "normal" results are fetched
    just concatenated the jsons from DB without parsing. This speeds
    up the processing significantly.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- contest/backend/query.py
+++ contest/backend/query.py
@@ -3,9 +3,12 @@
 
 
 from flask import Flask
+from flask import Response
 from flask import request
-import couchdb
+import json
+import psycopg2
 import os
+import re
 import datetime
 
 
@@ -13,17 +16,10 @@ app = Flask("NIPA contest query")
 
 user = os.getenv('DB_USER')
 pwd = os.getenv('DB_PWD')
-couch = couchdb.Server(f'http://{user}:{pwd}@127.0.0.1:5984')
-res_db = couch["results"]
 
-
-def branches_to_rows(br_cnt):
-    data = res_db.view('branch/rows', None,
-                       group=True, descending=True, limit=br_cnt)
-    cnt = 0
-    for row in data:
-        cnt += row.value
-    return cnt
+db_name = os.getenv('DB_NAME')
+psql = psycopg2.connect(database=db_name)
+psql.autocommit = True
 
 
 @app.route('/')
@@ -31,21 +27,73 @@ def hello():
     return '<h1>boo!</h1>'
 
 
+@app.route('/branches')
+def branches():
+    global psql
+
+    with psql.cursor() as cur:
+        cur.execute(f"SELECT branch, t_date, base, url FROM branches ORDER BY branch DESC LIMIT 40")
+        rows = [{"branch": r[0], "date": r[1].isoformat() + "+00:00", "base": r[2], "url": r[3]} for r in cur.fetchall()]
+        rows.reverse()
+    return rows
+
+
+def branches_to_rows(br_cnt, remote):
+    global psql
+
+    cnt = 0
+    with psql.cursor() as cur:
+        if remote:
+            q = f"SELECT branch,count(*),remote FROM results GROUP BY branch, remote ORDER BY branch DESC LIMIT {br_cnt}"
+        else:
+            q = f"SELECT branch,count(*) FROM results GROUP BY branch ORDER BY branch DESC LIMIT {br_cnt}"
+        cur.execute(q)
+        for r in cur.fetchall():
+            cnt += r[1]
+    return cnt
+
+
+def result_as_l2(raw):
+    row = json.loads(raw)
+    flat = []
+
+    for l1 in row["results"]:
+        if "results" not in l1:
+            flat.append(l1)
+        else:
+            for case in l1["results"]:
+                data = l1.copy()
+                del data["results"]
+                data |= case
+                data["test"] = l1["test"] + '.' + case["test"]
+                flat.append(data)
+    row["results"] = flat
+    return json.dumps(row)
+
+
 @app.route('/results')
 def results():
-    global couch
-
-    t1 = datetime.datetime.now()
+    global psql
 
     br_name = request.args.get('branch-name')
     if br_name:
+        if re.match(r'^[\w_ -]+$', br_name) is None:
+            return {}
+
         t1 = datetime.datetime.now()
-        rows = [r.value for r in res_db.view('branch/row_fetch', None,
-                                             key=br_name, limit=100)]
+        with psql.cursor() as cur:
+            cur.execute(f"SELECT json_normal FROM results WHERE branch = '{br_name}' LIMIT 100")
+            rows = [json.loads(r[0]) for r in cur.fetchall()]
         t2 = datetime.datetime.now()
         print("Query for exact branch took: ", str(t2-t1))
         return rows
 
+    t1 = datetime.datetime.now()
+
+    remote = request.args.get('remote')
+    if remote and re.match(r'^[\w_ -]+$', remote) is None:
+        remote = None
+    form = request.args.get('format')
     br_cnt = request.args.get('branches')
     try:
         br_cnt = int(br_cnt)
@@ -54,12 +102,47 @@ def results():
     if not br_cnt:
         br_cnt = 10
 
-    need_rows = branches_to_rows(br_cnt)
+    need_rows = branches_to_rows(br_cnt, remote)
+
     t2 = datetime.datetime.now()
-    data = [r.value for r in res_db.view('branch/row_fetch', None,
-                                         descending=True, limit=need_rows)]
+
+    where = f"WHERE remote = '{remote}'" if remote else ''
+    if not form or form == "normal":
+        with psql.cursor() as cur:
+            cur.execute(f"SELECT json_normal FROM results {where} ORDER BY branch DESC LIMIT {need_rows}")
+            rows = "[" + ",".join([r[0] for r in cur.fetchall()]) + "]"
+    elif form == "l2":
+        with psql.cursor() as cur:
+            cur.execute(f"SELECT json_normal, json_full FROM results {where} ORDER BY branch DESC LIMIT {need_rows}")
+            rows = "["
+            for r in cur.fetchall():
+                if rows[-1] != '[':
+                    rows += ','
+                if r[1] and len(r[1]) > 50:
+                    rows += result_as_l2(r[1])
+                else:
+                    rows += r[0]
+            rows += ']'
+    else:
+        rows = "[]"
 
     t3 = datetime.datetime.now()
     print(f"Query for {br_cnt} branches, {need_rows} records took: {str(t3-t1)} ({str(t2-t1)}+{str(t3-t2)})")
 
-    return data
+    return Response(rows, mimetype='application/json')
+
+
+@app.route('/remotes')
+def remotes():
+    global psql
+
+    t1 = datetime.datetime.now()
+
+    with psql.cursor() as cur:
+        cur.execute(f"SELECT remote FROM results GROUP BY remote LIMIT 50")
+        rows = [r[0] for r in cur.fetchall()]
+
+    t2 = datetime.datetime.now()
+    print(f"Query for remotes: {str(t2-t1)}")
+
+    return rows
