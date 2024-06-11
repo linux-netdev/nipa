2e33d65fc6b2166218d84416409069e6053c43ec
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2024-06-11 23:16:49 +0200

    contest: remote: export 'namify' to fetcher
    It sounds interesting to avoid using spaces and other unwanted chars in
    test names. This is currently done for the kselftests, but others might
    need the same treatment, e.g. in kUnit tests.
    
    To help with that, the 'namify' helper is exported to fetcher.py.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- contest/remote/lib/fetcher.py
+++ contest/remote/lib/fetcher.py
@@ -3,6 +3,7 @@
 import datetime
 import json
 import os
+import re
 import requests
 import subprocess
 import time
@@ -154,3 +155,10 @@ class Fetcher:
     def run(self):
         while self.life.next_poll():
             self._run_once()
+
+
+def namify(what):
+    name = re.sub(r'[^0-9a-zA-Z]+', '-', what)
+    if name[-1] == '-':
+        name = name[:-1]
+    return name
