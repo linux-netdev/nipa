472716ba45020b25bddeb2204857803857a3441c
Jakub Kicinski <kuba@kernel.org>
2024-06-12 08:12:25 -0700

    contest: lib: export namify
    We do selective imports, __init__ needs to import things.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- contest/remote/lib/__init__.py
+++ contest/remote/lib/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 
-from .fetcher import Fetcher
+from .fetcher import Fetcher, namify
 from .loadavg import wait_loadavg
 from .vm import VM, new_vm, guess_indicators
 from .cbarg import CbArg
