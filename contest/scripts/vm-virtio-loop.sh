faee581a0fe5a07ecc12d7b9aeac7828bb1dac70
Jakub Kicinski <kuba@kernel.org>
2025-04-16 16:29:39 -0700

    contest: switch to enp0sx names for virtio
    Use PCI virtio devices, they are more capable.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- contest/scripts/vm-virtio-loop.sh
+++ contest/scripts/vm-virtio-loop.sh
@@ -3,7 +3,10 @@
 
 # Expect we were booted into a virtme-ng VM with "--net loop"
 
-for ifc in eth0 eth1; do
+IFC0=enp0s1
+IFC1=enp0s2
+
+for ifc in $IFC0 $IFC1; do
     if ! ethtool -i "$ifc" | grep -q virtio; then
 	echo "Error: $ifc is not virtio"
 	exit 1
@@ -11,25 +14,25 @@ for ifc in eth0 eth1; do
 done
 
 ip netns add ns-remote
-ip link set dev eth1 netns ns-remote
+ip link set dev $IFC1 netns ns-remote
 export REMOTE_TYPE=netns
 export REMOTE_ARGS=ns-remote
 
-ip                  link set dev eth0 up
-ip -netns ns-remote link set dev eth1 up
-export NETIF=eth0
+ip                  link set dev $IFC0 up
+ip -netns ns-remote link set dev $IFC1 up
+export NETIF=$IFC0
 
-ip                  addr add dev eth0 192.0.2.1/24
-ip -netns ns-remote addr add dev eth1 192.0.2.2/24
+ip                  addr add dev $IFC0 192.0.2.1/24
+ip -netns ns-remote addr add dev $IFC1 192.0.2.2/24
 export  LOCAL_V4=192.0.2.1
 export REMOTE_V4=192.0.2.2
 
-ip                  addr add dev eth0 2001:db8::1/64 nodad
-ip -netns ns-remote addr add dev eth1 2001:db8::2/64 nodad
+ip                  addr add dev $IFC0 2001:db8::1/64 nodad
+ip -netns ns-remote addr add dev $IFC1 2001:db8::2/64 nodad
 export  LOCAL_V6=2001:db8::1
 export REMOTE_V6=2001:db8::2
 
-sysctl -w net.ipv6.conf.eth0.keep_addr_on_down=1
+sysctl -w net.ipv6.conf.$IFC0.keep_addr_on_down=1
 # We don't bring remote down, it'd break remote via SSH
 
 sleep 1
