440cfca27f409ae3a02dea9168c9e2b039b94041
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-06-04 16:47:19 +0200

    tests: pylint/shellcheck/yamllint: shellcheck compliant
    It sounds good to have the script checking for shellcheck compliance to
    be shellcheck compliant :)
    
    While at it, pylint and yamllint scripts have also be modified because
    they are inspired by the shellcheck one.
    
    Before the modifications, shellcheck was complaining about:
    
         53  note: Double quote to prevent globbing and word splitting. [SC2086]
          4  warning: Quote this to prevent word splitting. [SC2046]
          3  error: Argument mixes string and array. Use * or separate argument. [SC2145]
          2  warning: Use 'cd ... || exit' or 'cd ... || return' in case cd fails. [SC2164]
    
    Not any more.
    
    Also fix the indentation around the 'diff' at the end of each script,
    and in shellcheck.sh, s/Building/Checking/ like the others.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- tests/patch/yamllint/yamllint.sh
+++ tests/patch/yamllint/yamllint.sh
@@ -5,13 +5,13 @@ HEAD=$(git rev-parse HEAD)
 rc=0
 
 pr() {
-    echo " ====== $@ ======" | tee -a /dev/stderr
+    echo " ====== $* ======" | tee -a /dev/stderr
 }
 
 # If it doesn't touch .yaml files, don't bother. Ignore deleted.
 if ! git show --diff-filter=AM --pretty="" --name-only "${HEAD}" | grep -q -E "\.yaml$"
 then
-    echo "No YAML files touched, skip" >&$DESC_FD
+    echo "No YAML files touched, skip" >&"$DESC_FD"
     exit 0
 fi
 
@@ -30,35 +30,35 @@ git checkout -q HEAD~
 
 # Also ignore created, as not present in the parent commit
 for f in $(git show --diff-filter=M --pretty="" --name-only "${HEAD}" | grep -E "\.yaml$"); do
-    yamllint $f | tee -a $tmpfile_o
+    yamllint "$f" | tee -a "$tmpfile_o"
 done
 
-incumbent=$(grep -i -c " error " $tmpfile_o)
-incumbent_w=$(grep -i -c " warning " $tmpfile_o)
+incumbent=$(grep -i -c " error " "$tmpfile_o")
+incumbent_w=$(grep -i -c " warning " "$tmpfile_o")
 
 pr "Checking the tree with the patch"
-git checkout -q $HEAD
+git checkout -q "$HEAD"
 
 for f in $(git show --diff-filter=AM --pretty="" --name-only "${HEAD}" | grep -E "\.yaml$"); do
-    yamllint $f | tee -a $tmpfile_n
+    yamllint "$f" | tee -a "$tmpfile_n"
 done
 
-current=$(grep -i -c " error " $tmpfile_n)
-current_w=$(grep -i -c " warning " $tmpfile_n)
+current=$(grep -i -c " error " "$tmpfile_n")
+current_w=$(grep -i -c " warning " "$tmpfile_n")
 
-echo "Errors before: $incumbent (+warn: $incumbent_w) this patch: $current (+warn: $current_w)" >&$DESC_FD
+echo "Errors before: $incumbent (+warn: $incumbent_w) this patch: $current (+warn: $current_w)" >&"$DESC_FD"
 
-if [ $current_w -gt $incumbent_w ]; then
+if [ "$current_w" -gt "$incumbent_w" ]; then
     echo "New warnings added" 1>&2
 
     rc=250
 fi
 
-if [ $current -gt $incumbent ]; then
-  echo "New errors added" 1>&2
-  diff -U 0 $tmpfile_o $tmpfile_n 1>&2
+if [ "$current" -gt "$incumbent" ]; then
+    echo "New errors added" 1>&2
+    diff -U 0 "$tmpfile_o" "$tmpfile_n" 1>&2
 
-  rc=1
+    rc=1
 fi
 
 rm "$tmpfile_o" "$tmpfile_n"
