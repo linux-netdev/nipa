cb66c52a0b499d4e3dc68035026cde43264528c2
Matthieu Baerts (NGI0) <matttbe@kernel.org>
2025-06-04 16:47:19 +0200

    tests: pylint/shellcheck/yamllint: shellcheck compliant
    It sounds good to have the script checking for shellcheck compliance to
    be shellcheck compliant :)
    
    While at it, pylint and yamllint scripts have also be modified because
    they are inspired by the shellcheck one.
    
    Before the modifications, shellcheck was complaining about:
    
         53  note: Double quote to prevent globbing and word splitting. [SC2086]
          4  warning: Quote this to prevent word splitting. [SC2046]
          3  error: Argument mixes string and array. Use * or separate argument. [SC2145]
          2  warning: Use 'cd ... || exit' or 'cd ... || return' in case cd fails. [SC2164]
    
    Not any more.
    
    Also fix the indentation around the 'diff' at the end of each script,
    and in shellcheck.sh, s/Building/Checking/ like the others.
    
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>

--- tests/patch/pylint/pylint.sh
+++ tests/patch/pylint/pylint.sh
@@ -5,13 +5,13 @@ HEAD=$(git rev-parse HEAD)
 rc=0
 
 pr() {
-    echo " ====== $@ ======" | tee -a /dev/stderr
+    echo " ====== $* ======" | tee -a /dev/stderr
 }
 
 # If it doesn't touch .py files, don't bother. Ignore deleted.
 if ! git show --diff-filter=AM --pretty="" --name-only "${HEAD}" | grep -q -E "\.py$"
 then
-    echo "No python scripts touched, skip" >&$DESC_FD
+    echo "No python scripts touched, skip" >&"$DESC_FD"
     exit 0
 fi
 
@@ -30,35 +30,35 @@ git checkout -q HEAD~
 
 # Also ignore created, as not present in the parent commit
 for f in $(git show --diff-filter=M --pretty="" --name-only "${HEAD}" | grep -E "\.py$"); do
-    pylint $f | tee -a $tmpfile_o
+    pylint "$f" | tee -a "$tmpfile_o"
 done
 
-incumbent=$(grep -i -c ": E[0-9][0-9][0-9][0-9]: " $tmpfile_o)
-incumbent_w=$(grep -i -c ": [WC][0-9][0-9][0-9][0-9]: " $tmpfile_o)
+incumbent=$(grep -i -c ": E[0-9][0-9][0-9][0-9]: " "$tmpfile_o")
+incumbent_w=$(grep -i -c ": [WC][0-9][0-9][0-9][0-9]: " "$tmpfile_o")
 
 pr "Checking the tree with the patch"
-git checkout -q $HEAD
+git checkout -q "$HEAD"
 
 for f in $(git show --diff-filter=AM --pretty="" --name-only "${HEAD}" | grep -E "\.py$"); do
-    pylint $f | tee -a $tmpfile_n
+    pylint "$f" | tee -a "$tmpfile_n"
 done
 
-current=$(grep -i -c ": E[0-9][0-9][0-9][0-9]: " $tmpfile_n)
-current_w=$(grep -i -c ": [WC][0-9][0-9][0-9][0-9]: " $tmpfile_n)
+current=$(grep -i -c ": E[0-9][0-9][0-9][0-9]: " "$tmpfile_n")
+current_w=$(grep -i -c ": [WC][0-9][0-9][0-9][0-9]: " "$tmpfile_n")
 
-echo "Errors before: $incumbent (+warn: $incumbent_w) this patch: $current (+warn: $current_w)" >&$DESC_FD
+echo "Errors before: $incumbent (+warn: $incumbent_w) this patch: $current (+warn: $current_w)" >&"$DESC_FD"
 
-if [ $current_w -gt $incumbent_w ]; then
+if [ "$current_w" -gt "$incumbent_w" ]; then
     echo "New warnings added" 1>&2
 
     rc=250
 fi
 
-if [ $current -gt $incumbent ]; then
-  echo "New errors added" 1>&2
-  diff -U 0 $tmpfile_o $tmpfile_n 1>&2
+if [ "$current" -gt "$incumbent" ]; then
+    echo "New errors added" 1>&2
+    diff -U 0 "$tmpfile_o" "$tmpfile_n" 1>&2
 
-  rc=1
+    rc=1
 fi
 
 rm "$tmpfile_o" "$tmpfile_n"
--- tests/patch/shellcheck/shellcheck.sh
+++ tests/patch/shellcheck/shellcheck.sh
@@ -5,13 +5,13 @@ HEAD=$(git rev-parse HEAD)
 rc=0
 
 pr() {
-    echo " ====== $@ ======" | tee -a /dev/stderr
+    echo " ====== $* ======" | tee -a /dev/stderr
 }
 
 # If it doesn't touch .sh files, don't bother. Ignore deleted.
 if ! git show --diff-filter=AM --pretty="" --name-only "${HEAD}" | grep -q -E "\.sh$"
 then
-    echo "No shell scripts touched, skip" >&$DESC_FD
+    echo "No shell scripts touched, skip" >&"$DESC_FD"
     exit 0
 fi
 
@@ -31,43 +31,43 @@ git checkout -q HEAD~
 # Also ignore created, as not present in the parent commit
 for f in $(git show --diff-filter=M --pretty="" --name-only "${HEAD}" | grep -E "\.sh$"); do
     (
-	sha=$(echo $f | sha256sum | awk '{print $1}')
+	sha=$(echo "$f" | sha256sum | awk '{print $1}')
 	echo "Checking $f - $sha"
 	echo
 
-	cd $(dirname $f)
+	cd "$(dirname "$f")" || exit 1
 	sha="${tmpfile_o}_${sha}"
 	rm -f "${sha}"
-	shellcheck -x $(basename $f) | tee -a "${tmpfile_o}" "${sha}"
+	shellcheck -x "$(basename "$f")" | tee -a "${tmpfile_o}" "${sha}"
 	echo
     )
 done
 
 # ex: SC3045 (warning): In POSIX sh, printf -v is undefined.
 # severity: error, warning, info, style
-incumbent=$(grep -c " (error):" $tmpfile_o)
-incumbent_w=$(grep -c " (warning):" $tmpfile_o)
+incumbent=$(grep -c " (error):" "$tmpfile_o")
+incumbent_w=$(grep -c " (warning):" "$tmpfile_o")
 
-pr "Building the tree with the patch"
-git checkout -q $HEAD
+pr "Checking the tree with the patch"
+git checkout -q "$HEAD"
 
 for f in $(git show --diff-filter=AM --pretty="" --name-only "${HEAD}" | grep -E "\.sh$"); do
     (
-	sha=$(echo $f | sha256sum | awk '{print $1}')
+	sha=$(echo "$f" | sha256sum | awk '{print $1}')
 	echo "Checking $f - $sha"
 	echo
 
-	cd $(dirname $f)
+	cd "$(dirname "$f")" || exit 1
 	sha="${tmpfile_n}_${sha}"
 	rm -f "${sha}"
-	shellcheck -x $(basename $f) | tee -a "${tmpfile_n}" "${sha}"
+	shellcheck -x "$(basename "$f")" | tee -a "${tmpfile_n}" "${sha}"
 	echo
     )
 done
 
 # severity: error, warning, info, style
-current=$(grep -c " (error):" $tmpfile_n)
-current_w=$(grep -c " (warning):" $tmpfile_n)
+current=$(grep -c " (error):" "$tmpfile_n")
+current_w=$(grep -c " (warning):" "$tmpfile_n")
 
 # if a file was compliant before or is new, mark everything as error to keep it good.
 for f in "${tmpfile_n}_"*; do
@@ -88,19 +88,19 @@ for f in "${tmpfile_n}_"*; do
     current=$((current + extra))
 done
 
-echo "Errors before: $incumbent (+warn: $incumbent_w) this patch: $current (+warn: $current_w)" >&$DESC_FD
+echo "Errors before: $incumbent (+warn: $incumbent_w) this patch: $current (+warn: $current_w)" >&"$DESC_FD"
 
-if [ $current_w -gt $incumbent_w ]; then
+if [ "$current_w" -gt "$incumbent_w" ]; then
     echo "New warnings added" 1>&2
 
     rc=250
 fi
 
-if [ $current -gt $incumbent ]; then
-  echo "New errors added" 1>&2
-  diff -U 0 $tmpfile_o $tmpfile_n 1>&2
+if [ "$current" -gt "$incumbent" ]; then
+    echo "New errors added" 1>&2
+    diff -U 0 "$tmpfile_o" "$tmpfile_n" 1>&2
 
-  rc=1
+    rc=1
 fi
 
 rm "$tmpfile_o"* "$tmpfile_n"*
