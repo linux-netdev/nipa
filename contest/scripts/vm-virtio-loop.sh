238e7a9338a2437580f6e1a65d6b3b7223d72713
Jakub Kicinski <kuba@kernel.org>
2025-06-03 18:12:24 -0700

    contest: virtio: don't use the same addresses as ksft
    In my infinite wisdom I copied the addresses which netdevsim
    uses in ksft. But some tests assume these addresses are unused
    and want to use these them on tunnels.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- contest/scripts/vm-virtio-loop.sh
+++ contest/scripts/vm-virtio-loop.sh
@@ -22,15 +22,15 @@ ip                  link set dev $IFC0 up
 ip -netns ns-remote link set dev $IFC1 up
 export NETIF=$IFC0
 
-ip                  addr add dev $IFC0 192.0.2.1/24
-ip -netns ns-remote addr add dev $IFC1 192.0.2.2/24
-export  LOCAL_V4=192.0.2.1
-export REMOTE_V4=192.0.2.2
+ip                  addr add dev $IFC0 192.0.3.1/24
+ip -netns ns-remote addr add dev $IFC1 192.0.3.2/24
+export  LOCAL_V4=192.0.3.1
+export REMOTE_V4=192.0.3.2
 
-ip                  addr add dev $IFC0 2001:db8::1/64 nodad
-ip -netns ns-remote addr add dev $IFC1 2001:db8::2/64 nodad
-export  LOCAL_V6=2001:db8::1
-export REMOTE_V6=2001:db8::2
+ip                  addr add dev $IFC0 2001:db8:1::1/64 nodad
+ip -netns ns-remote addr add dev $IFC1 2001:db8:1::2/64 nodad
+export  LOCAL_V6=2001:db8:1::1
+export REMOTE_V6=2001:db8:1::2
 
 sysctl -w net.ipv6.conf.$IFC0.keep_addr_on_down=1
 # We don't bring remote down, it'd break remote via SSH
